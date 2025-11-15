dd6d6a4ad64381705f6d50e1e4b56e7376d0a271
GKORDS <master24939@gmail.com>
2025-11-15 19:57:49 +0700

    Add Winona infinite buff mod

---
+++ modinfo.lua
@@ -0,0 +1,15 @@
+name = "Winona Infinite Buff"
+description = [[Buffs Winona's Remote and Catapult so that all of their attributes become effectively infinite, except for their damage.]]
+author = "AI Assistant"
+version = "1.0.0"
+forumthread = ""
+icon_atlas = ""
+icon = ""
+
+api_version = 10
+
+all_clients_require_mod = false
+client_only_mod = false
+server_filter_tags = { "winona", "buff", "infinite" }
+
+priority = 0
